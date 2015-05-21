8169b01391d30114dfb0f10398616fbcc0c046cc
himkt <himkt@klis.tsukuba.ac.jp>
2015-05-21 18:19:09 +0900

    use sine and cosine to normalization

--- calc_porality.rb
+++ calc_porality.rb
@@ -9,10 +9,29 @@ polarity = []
 
 nm.parse(gets.chomp).split(/\n/).each do |line|
   break if line == 'EOS'
-  value = db.execute("SELECT value FROM models WHERE word = ?",line.split(/\t/)[0])[0]
-  polarity << value[0] if value
+  value = db.execute("SELECT value FROM models WHERE word = ?",line.split(/\t/)[1].split(/\,/)[6])[0]
+  polarity << [value[0]] if value
+end
+
+# it have not been calculated well yet.
+# I puts these value as a sine value on an unit circle
+# Because Data exists from -1 to 1, I use only first quadrant and fourth quadrant.
+polarity.each_with_index do |sine,i|
+  cosine = (Math.sqrt(1 - sine[0]**2))
+  polarity[i] << cosine
+end
+
+p polarity
+
+item = polarity.pop
+
+composition_sine = item[0]
+composition_cosine = item[1]
+
+polarity.each do |elem|
+  composition_sine = composition_sine * item[1] + composition_cosine * item[0]
+  composition_cosine = (Math.sqrt(1 - composition_sine**2))
 end
 
 
-# it have not been calculated well yet.
-p polarity
+p composition_sine
