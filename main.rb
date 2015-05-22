0771d89203c6159f82a24bffb5efa976476bb9de
himkt <himkt@klis.tsukuba.ac.jp>
2015-05-22 16:05:03 +0900

    third person singular present...

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 require './script/calc_porality'
 
-print "please input sentense: "
+print "please input a sentence: "
 sentense = gets.chomp
 
 puts "value is #{Pos_Neg::calc sentense}. (this value have from -1 to 1)"
