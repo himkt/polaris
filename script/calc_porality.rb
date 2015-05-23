fe09e7ac339eff74b8387b0d6f2153da5222e113
himkt <himkt@klis.tsukuba.ac.jp>
2015-05-23 15:36:45 +0900

    use tanh to calc the value

--- script/calc_porality.rb
+++ script/calc_porality.rb
@@ -9,38 +9,18 @@ module Pos_Neg
     nm = Natto::MeCab.new
     db = SQLite3::Database.new("data/model.db")
 
-    polarity = []
+    sum = 0
 
     nm.parse(sentense).split(/\n/).each do |line|
       elem = line.split(/\t/)
       break if elem[0] == 'EOS' 
       word = (elem[1].split(/\,/)[6] == '*') ? elem[0] : elem[1].split(/\,/)[6]
       value = db.execute("SELECT value FROM models WHERE word = ? LIMIT 1",word)[0]
-      polarity << [value[0]] if value
+      sum += value[0] if value
     end
 
-    # it have not been calculated well yet.
-    # I puts these value as a sine value on an unit circle
-    # Because Data exists from -1 to 1, I use only first quadrant and fourth quadrant.
-    polarity.each_with_index do |sine,i|
-      cosine = (Math.sqrt(1 - sine[0]**2))
-      polarity[i] << cosine
-    end
-
-    item = polarity.pop
-
-    if item
-      composition_sine = item[0]
-      composition_cosine = item[1]
-      polarity.each do |elem|
-        composition_sine = composition_sine * elem[1] + composition_cosine * elem[0]
-        composition_cosine = (Math.sqrt(1 - composition_sine**2))
-      end
-    else
-      composition_sine = 0
-    end
-
-    return composition_sine
+    e = Math.exp(1)
+    return (e ** sum - e** (-1 * sum)) / (e ** sum + e** (-1 * sum))
     # "value is #{composition_sine}. (this value have from -1 to 1)"
 
   end
