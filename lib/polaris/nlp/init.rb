17aec7c01c322b18b951ada21f68e102d75a5ed2
himkt <himkt@klis.tsukuba.ac.jp>
2015-09-06 13:19:33 +0900

    implement init

--- lib/polaris/nlp/init.rb
+++ lib/polaris/nlp/init.rb
@@ -16,12 +16,32 @@ module Polaris
       unless Dir.exists?("#{@home}/.polaris")
         Dir.mkdir("#{@home}/.polaris")
       end
-      ja_text = open('http://www.lr.pi.titech.ac.jp/%7Etakamura/pubs/pn_ja.dic').read.encode('utf-8')
-      en_text = open('http://www.lr.pi.titech.ac.jp/%7Etakamura/pubs/pn_en.dic').read.encode('utf-8')
+      ja_text = open('http://www.lr.pi.titech.ac.jp/%7Etakamura/pubs/pn_ja.dic').read.encode('utf-8','sjis')
+      en_text = open('http://www.lr.pi.titech.ac.jp/%7Etakamura/pubs/pn_en.dic').read.encode('utf-8','sjis')
 
-      unless File.exists?("#{@home}/.polaris/features.sqlite3")
+      db = SQLite3::Database.new("#{@home}/.polaris/features.sqlite3")
+      db.execute("DROP TABLE models")
+      db.execute("CREATE TABLE models (word TEXT ,value REAL);")
+
+
+      ja_text.split("\n").each do |line|
+        item = line.split(':')
+        word = item[0]
+        value = item[3]
+        insert_value(db,word,value)
       end
 
+      en_text.split("\n").each do |line|
+        item = line.split(':')
+        word = item[0]
+        value = item[2]
+        insert_value(db,word,value)
+      end
+
+    end
+
+    def insert_value(db,word,value)
+      return db.execute("INSERT INTO models (word, value) VALUES('#{word}', #{value});")
     end
 
     def clean
